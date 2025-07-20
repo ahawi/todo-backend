d04b70a20e285910eda84c9d3eb61df2182c2eeb
ahawi <nastya.murlla@gmail.com>
2025-07-20 09:44:27 +0300

    Исправление для работы бекенда

--- dist/app.js
+++ dist/app.js
@@ -11,4 +11,4 @@ const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use('/tasks', taskRoutes_1.default);
-module.exports = app;
+exports.default = app;
